02e65979c3a77f9d5f203a429b30b3d9c3064f30
likanug <likanug.g@qq.com>
2026-01-19 19:24:32 +0800

    feat(i18n): allow dynamic URL interpolation in about page markdown

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 import { useHead } from '@vueuse/head';
+import { config } from '@/config';
 
 useHead({ title: 'About - IT Tools' });
+
+const links = {
+  authorUrl: `[likanug](${config.app.authorUrl})`,
+  sponsoringUrl: config.app.sponsoringUrl,
+  repositoryUrl: config.app.repositoryUrl,
+};
 </script>
 
 <template>
-  <c-markdown :markdown="$t('about.content')" mx-auto mt-50px max-w-600px />
+  <c-markdown :markdown="$t('about.content', links)" mx-auto mt-50px max-w-600px />
 </template>
