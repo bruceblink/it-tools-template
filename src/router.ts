c83957b9e41805cae84e98e5eb2d4136f37db6ab
likanug <likanug.g@qq.com>
2026-01-24 21:31:34 +0800

    style: format code

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import { layouts } from '@/layouts';
 import HomePage from './pages/Home.page.vue';
 import NotFound from './pages/404.page.vue';
 import { tools } from './tools';
 import { config } from './config';
 import { routes as demoRoutes } from './ui/demo/demo.routes';
+import { layouts } from '@/layouts';
 
 const toolsRoutes = tools.map(({ path, name, component, ...config }) => ({
   path,
@@ -15,7 +15,7 @@ const toolsRoutes = tools.map(({ path, name, component, ...config }) => ({
 const toolsRedirectRoutes = tools
   .filter(({ redirectFrom }) => redirectFrom && redirectFrom.length > 0)
   .flatMap(
-    ({ path, redirectFrom }) => redirectFrom?.map((redirectSource) => ({ path: redirectSource, redirect: path })) ?? [],
+    ({ path, redirectFrom }) => redirectFrom?.map(redirectSource => ({ path: redirectSource, redirect: path })) ?? [],
   );
 
 const router = createRouter({
