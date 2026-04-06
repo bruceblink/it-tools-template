1735e97483ebd5516564f354d6834b0e75d01ab7
likanug <likanug.g@qq.com>
2026-04-06 19:13:55 +0800

    fix: resolve undefined links on about page

--- src/ui/c-markdown/c-markdown.vue
+++ src/ui/c-markdown/c-markdown.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { marked } from 'marked';
+import { computed, toRefs } from 'vue';
+import { marked, type Tokens } from 'marked';
 import DomPurify from 'dompurify';
 
 const props = withDefaults(defineProps<{ markdown?: string }>(), { markdown: '' });
@@ -7,13 +8,14 @@ const { markdown } = toRefs(props);
 
 marked.use({
   renderer: {
-    link(href, title, text) {
-      return `<a class="text-primary transition decoration-none hover:underline" href="${href}" target="_blank" rel="noopener">${text}</a>`;
+    link({ href, text, title }: Tokens.Link) {
+      const titleAttribute = title ? ` title="${title}"` : '';
+      return `<a class="text-primary transition decoration-none hover:underline" href="${href}"${titleAttribute} target="_blank" rel="noopener">${text}</a>`;
     },
   },
 });
 
-const html = computed(() => DomPurify.sanitize(marked(markdown.value), { ADD_ATTR: ['target'] }));
+const html = computed(() => DomPurify.sanitize(marked.parse(markdown.value) as string, { ADD_ATTR: ['target'] }));
 </script>
 
 <template>
